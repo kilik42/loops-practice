eb4ffc92e62e0cabdd4a57bf56abd954c8d97a14
Evins <MAEvins@cps.k12.il.us>
2024-05-06 09:56:22 -0500

    loops

---
+++ loops.py
@@ -0,0 +1,54 @@
+
+#####################################while loops###########################
+# problem #1
+# Some people have just a first name and a last name. Some people also have a middle name. Some people have five middle names.
+
+# Write a program that asks the user how many names they have. (If they have a first name, two middle names, and a last name, for example, they would type 4.) Then, using a for loop, ask the user for each of their names. Finally, print their full name.
+
+
+
+
+
+
+# problem #2
+# Write a program that prints even numbers 2 through 20 using a while loop.
+
+
+
+
+
+#####################################for loops###########################
+
+# problem #3
+# Write a program that prints the numbers 1 through 10 using a for loop.
+
+
+
+#problem #4
+# Write a program that prints the numbers 1 through 10 using a for loop. Use an if statement to print only the odd numbers.
+
+
+
+
+#problem #5
+# Write a program that prints the numbers 1 through 10 using a for loop. Use an if statement to print only the even numbers.
+
+
+
+
+#problem #7
+# Write a program that asks the user for three test scores. The program should then report the average of these three scores. You will need:
+
+# A call to input. You should only have one line that has this!
+# A for loop.
+# Arithmetic operators.
+
+
+
+#problem #8
+# # Requirements:
+# Write a program that asks the user for three test scores. The program should then report the average of these three scores. You will need:
+
+# A call to input. You should only have one line that has this!
+# A for loop.
+# Arithmetic operators.
